d85f02702e71e1d71bd5b192c6b34ab5575ed555
ainyyyyyyyyyy <105438255+ainyyyyyyyyyy@users.noreply.github.com>
2022-12-12 20:02:44 +0300

    Add dynamic db generation

--- create.py
+++ create.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, sessionmaker
 import psycopg2
+import load
+from transliterate import translit
 
 from parse import GoogleSheet
 
@@ -10,31 +12,41 @@ Base = declarative_base()
 
 
 class UserData(Base):
+    
+    googleSheet = GoogleSheet()
+    ruGroupList = googleSheet.get_groups_of_students()
+    groupList = []
+    #транслит + очистка строки от лишних символов
+    for gr in ruGroupList:
+        translitGr = translit(gr, language_code='ru', reversed=True)
+        clearGr = "".join(c for c in translitGr if c.isalpha())
+        groupList.append(clearGr)
 
     __tablename__ = "user_data"
     name = Column(String, primary_key=True)
-    #userID = Column(BigInteger)
     parallel = Column(Integer)
     group = Column(String)
     classTeacher = Column(String)
     tutor = Column(String)
     
-    olymp = Column(Boolean)
-    noProfOrient = Column(Boolean)    
-    etc = Column(Boolean)     
-    etc2 = Column(Boolean)
+    #динамическая генерация кода для создания n-ного количества столбцов бд
+    newColumns = ""
+    for gr in groupList:
+        newColumns += (gr + " = Column(Boolean)\n")
+
+    exec(newColumns)
 
 
 def create():
-    engine = create_engine("postgresql://postgres:VladMurat228@/postgres")#на место postgres:1111 свой логин и пароль
+    engine = create_engine("postgresql://postgres:1111@/postgres")#на место postgres:1111 свой логин и пароль
     conn = engine.connect()
     conn.execute("commit")
-    conn.execute("create database db_school_v2")
+    conn.execute("create database db_school_v21")
     conn.close()
 
 
 def create_session():
-    engine = create_engine("postgresql://postgres:VladMurat228@localhost:5432/db_school_v2")  # сюда тоже
+    engine = create_engine("postgresql://postgres:1111@localhost:5432/db_school_v21")  # сюда тоже
     engine.connect()
     Base.metadata.create_all(engine)
 
@@ -44,3 +56,18 @@ def create_session():
     return session
 
 
+
+if __name__ == "__main__":     
+    googleSheet = GoogleSheet()
+    ruGroupList = googleSheet.get_groups_of_students()    
+    data = googleSheet.read_data()
+    groupList = []
+    
+    #транслит + очистка строки от лишних символов
+    for gr in ruGroupList:
+        translitGr = translit(gr, language_code='ru', reversed=True)
+        clearGr = "".join(c for c in translitGr if c.isalpha())
+        groupList.append(clearGr)
+
+    create()
+    load.loadData(create_session(), groupList, data)
